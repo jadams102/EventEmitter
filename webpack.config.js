1ae5b9d819f143dc965af8eebcf9ca73f0e7ec4b
Joel Adams <joeladamsdesign@gmail.com>
2018-08-14 14:53:17 -0700

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devtool: 'eval-source-map',
+  devServer: {
+    contentBase: './dist'
+  },
+  plugins: [
+    new UglifyJsPlugin({ sourceMap: true }),   
+    new CleanWebpackPlugin(['dist'])]
+};
