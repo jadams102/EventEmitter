54342bab4b7bc135db5138d43864011a5d978656
Joel Adams <joeladamsdesign@gmail.com>
2018-08-15 15:34:37 -0700

    add event emitter class to main.js

---
+++ main.js
@@ -0,0 +1,49 @@
+class EventEmitter {
+    constructor() {
+        this.events = {};
+    }
+
+    register(name, callback) {
+        if(this.events[name]) {
+            this.event[name].push(callback);
+        } else {
+            this.events[name] = [callback]
+            this.events[name].oneTime = false;
+        }
+
+    }
+
+    registerOneTime(name, callback) {
+        if(this.events[name]) {
+            this.events[name].oneTime = true;
+        } else {
+            this.events[name] = [callback];
+            this.events[name].oneTime = true;
+        }
+
+    }
+
+    trigger(name, ...rest) {
+        if (this.events[name]) {
+            this.events[name].forEach(cb =>
+            cb.apply(rest)
+            );
+        }
+
+        if(this.events[name].oneTime) {
+            delete this.events[name]
+        }
+    }
+
+    remove(name) {
+        if(this.events[name]) {
+            delete this.events[name]
+        } else {
+            console.log("Event not registered")
+        }  
+    }
+
+    removeAll() {
+        this.events = {};
+    }
+}
