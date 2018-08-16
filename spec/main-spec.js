1da09f14efb7df07bfacd2d4ee8c5f1b5748fed4
Joel Adams <joeladamsdesign@gmail.com>
2018-08-16 16:19:58 -0700

    add third test, update previous tests to fix function error

--- spec/main-spec.js
+++ spec/main-spec.js
@@ -3,19 +3,36 @@ import { EventEmitter } from '../src/main';
 describe('EventEmitter', function() {
     it('should register a named event', function() {
         const ee = new EventEmitter();
-        ee.register('click', () => function() {
+
+        ee.register('click', () => {
             console.log('test one');
         });
+
         expect (ee.events['click']).toBeDefined();
         expect (ee.events['click'].oneTime).toEqual(false);
     });
 
     it('should register a one-time event', function() {
         const ee = new EventEmitter();
-        ee.registerOneTime('click', () => function() {
-            console.log('test one');
+
+        ee.registerOneTime('click', () => {
+            console.log('test two');
         });
+
         expect (ee.events['click']).toBeDefined();
         expect (ee.events['click'].oneTime).toEqual(true);
     });
+
+    it('should trigger an event', function() {
+        let result = 'test';
+        const ee = new EventEmitter();
+        ee.register('click', () => {
+            result = 'result';
+        });
+
+        ee.trigger('click');
+
+        expect (result).toEqual('result');
+    });
+
 });
