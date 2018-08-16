c8a4ed5d98a8e2d0acfff70c4af1ce59aa6bbeb4
Joel Adams <joeladamsdesign@gmail.com>
2018-08-16 12:58:50 -0700

    add second test

--- spec/main-spec.js
+++ spec/main-spec.js
@@ -7,5 +7,15 @@ describe('EventEmitter', function() {
             console.log('test one');
         });
         expect (ee.events['click']).toBeDefined();
+        expect (ee.events['click'].oneTime).toEqual(false);
+    });
+
+    it('should register a one-time event', function() {
+        const ee = new EventEmitter();
+        ee.registerOneTime('click', () => function() {
+            console.log('test one');
+        });
+        expect (ee.events['click']).toBeDefined();
+        expect (ee.events['click'].oneTime).toEqual(true);
     });
 });
